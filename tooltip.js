a12fe8dadfa046fe0cf1f23f37b0007c1b95e612
Declan Sharkey <declan.sharkey@live.ie>
2014-06-11 21:46:18 +0100

    jQuery Tooltip

---
+++ tooltip.js
@@ -0,0 +1,29 @@
+//Tooltip
+//2014
+
++function($) {
+    $.fn.tooltip = function(){
+            return $(this).on("mousemove", "[data-tooltip]", function(event){
+                var popup = $(event.target).next(".popup")
+
+                
+                //if popup doesn't exist, build one
+                if (!popup.length) {
+                    var text = $(event.target).attr("data-tooltip")
+                    popup = $(event.target).after("<div class='popup' style='display: none;'>" + text+ "</div>").find(".popup")
+                }
+
+                //popup position in realtion to cursor
+                popup.css({
+                    top: event.pageY + 5 + "px",
+                    left: event.pageX + 5 + "px"
+                }).show();
+                
+                
+                }).on("mouseout", "[data-tooltip]", function(event) {
+                    $(event.target).next(".popup").hide();
+            });
+        }
+       
+       
+   }(jQuery);
